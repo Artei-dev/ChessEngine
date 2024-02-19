c08134e5973f360c0b5e3cda17242f258714f343
Mateusz Kuźmicki <mateusz.kuzmicki6@gmail.com>
2024-02-19 18:22:03 +0100

    ADD Basic board piece manipulation funcionality

--- src/main/java/ChessBoard.java
+++ src/main/java/ChessBoard.java
@@ -1,23 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessBoard {
     private Piece[][] board = new Piece[8][8];
+    private List<Piece> whiteCapturedPieces = new ArrayList<>();
+    private List<Piece> blackCapturedPieces = new ArrayList<>();
     public void addPiece(String position, Piece piece) {
         Coordinates coords = new Coordinates(position);
+        piece.setPosition(position);
         board[coords.getX()][coords.getY()] = piece;
     }
-
     public Piece getPiece(String position) {
         Coordinates coords = new Coordinates(position);
         return board[coords.getX()][coords.getY()];
     }
-    public Piece deletePiece(String position) {
+    public void deletePiece(String position) {
         Coordinates coords = new Coordinates(position);
-        Piece piece = getPiece(position);
         board[coords.getX()][coords.getY()] = null;
-        return piece;
+    }
+    private void capturePiece(String position) {
+        Piece piece = getPiece(position);
+        if(piece != null) {
+            switch (piece.color) {
+                case Color.BLACK -> whiteCapturedPieces.add(piece);
+                case Color.WHITE -> blackCapturedPieces.add(piece);
+            }
+        }
+    }
+    public void movePiece(String position1, String position2) {
+        Piece piece1 = getPiece(position1);
+        Piece piece2 = getPiece(position2);
+
+        boolean sameColor = piece2 != null && piece2.color == piece1.color;
+        if(piece1.getLegalMoves().contains(new Coordinates(position2)) && !sameColor) {
+            deletePiece(position1);
+            capturePiece(position2);
+            addPiece(position2, piece1);
+        }
     }
 
-    public void movePiece(String position1, String position2) {
-        Piece piece = deletePiece(position1);
-        addPiece(position2, piece);
+    public List<Piece> getWhiteCapturedPieces() {
+        return whiteCapturedPieces;
+    }
+
+    public List<Piece> getBlackCapturedPieces() {
+        return blackCapturedPieces;
     }
 }
--- src/test/java/ChessBoardTest.java
+++ src/test/java/ChessBoardTest.java
@@ -23,10 +23,50 @@ class ChessBoardTest {
     void canMoveC1KnightToD3() {
         ChessBoard chessBoard = new ChessBoard();
         Knight knight = new Knight(Color.WHITE);
-        chessBoard.addPiece("a1", knight);
-        assertEquals(chessBoard.getPiece("a1"), knight);
-        chessBoard.movePiece("a1", "d3");
-        assertNull(chessBoard.getPiece("a1"));
+        chessBoard.addPiece("c1", knight);
+        assertEquals(chessBoard.getPiece("c1"), knight);
+        chessBoard.movePiece("c1", "d3");
         assertEquals(chessBoard.getPiece("d3"), knight);
+        assertNull(chessBoard.getPiece("a1"));
+    }
+    @Test
+    void canNotMoveKnightToInvalidSquare() {
+        ChessBoard chessBoard = new ChessBoard();
+        Knight knight = new Knight(Color.WHITE);
+        chessBoard.addPiece("c1", knight);
+        assertEquals(chessBoard.getPiece("c1"), knight);
+        chessBoard.movePiece("c1", "d4");
+        assertEquals(chessBoard.getPiece("c1"), knight);
+        assertNull(chessBoard.getPiece("d4"));
+    }
+
+    @Test
+    void canNotMoveKnightToFriendlyOccupiedSquare() {
+        ChessBoard chessBoard = new ChessBoard();
+        Knight knight = new Knight(Color.WHITE);
+        Knight knight1 = new Knight(Color.WHITE);
+        chessBoard.addPiece("c1", knight);
+        chessBoard.addPiece("d3", knight1);
+        assertEquals(chessBoard.getPiece("c1"), knight);
+        assertEquals(chessBoard.getPiece("d3"), knight1);
+        chessBoard.movePiece("c1", "d3");
+        assertEquals(chessBoard.getPiece("c1"), knight);
+        assertEquals(chessBoard.getPiece("d3"), knight1);
+    }
+
+    @Test
+    void canMoveC1KnightToEnemyOccupiedSquare() {
+
+        ChessBoard chessBoard = new ChessBoard();
+        Piece knight = new Knight(Color.WHITE);
+        Piece pawn = new Pawn(Color.BLACK);
+        chessBoard.addPiece("c1", knight);
+        chessBoard.addPiece("d3", pawn);
+        assertEquals(chessBoard.getPiece("c1"), knight);
+        assertEquals(chessBoard.getPiece("d3"), pawn);
+        chessBoard.movePiece("c1", "d3");
+        assertNull(chessBoard.getPiece("c1"));
+        assertEquals(chessBoard.getPiece("d3"), knight);
+        assertTrue(chessBoard.getWhiteCapturedPieces().contains(pawn));
     }
 }
